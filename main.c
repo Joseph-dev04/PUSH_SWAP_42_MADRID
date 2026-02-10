dc560f2c3b33bf2547cffb2992b50eb84380ce0a
ATM <207956068+Jaitowerr@users.noreply.github.com>
2026-02-10 18:26:39 +0100

    parse.c y main.c

--- main.c
+++ main.c
@@ -3,39 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jopajuel <jopajuel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aitorres <aitorres@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 09:31:38 by jopajuel          #+#    #+#             */
-/*   Updated: 2026/02/10 12:40:28 by jopajuel         ###   ########.fr       */
+/*   Updated: 2026/02/10 18:23:53 by aitorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-int	main(int arc, char **argv)
-{
-	t_list	*a;
-	t_list	*b;
+// int	main(int arc, char **argv)
+// {
+// 	t_list	*a;
+// 	t_list	*b;
+// 	int		i;
+	
+// 	a = NULL;
+// 	b = NULL;
+// 	if (arc != 1)
+// 	{
+// 		i = 0;
+// 		if (ft_get_num(argv[i], &a))
+// 		{
+// 			// ft_sa(&a);
+// 			while (a)
+// 			{
+// 				printf("%i\n", a->value);
+// 				a = a->next;
+// 			}
+// 		}
+// 		else
+// 		{
+// 			// ft_free(a);
+// 			return (-1);
+// 		}
+// 	}
+// 	return (0);
+// }
 
-	a = NULL;
+int main(int arc, char **argv)
+{
+    t_list  *a;
+    t_list  *b; 
+    int     i;
+    
+    a = NULL;
 	b = NULL;
-	if (arc == 2)
-	{
-		if (ft_get_num(argv[1], &a))
-		{
-			//ft_sa(&a);
-			while (a)
-			{
-				printf("%i\n", a->value);
-				a = a->next;
-			}
-		}
-		else
-		{
-			ft_free(a);
-			return (-1);
-		}
-	}
-	return (0);
+    if (arc > 1)
+    {
+        i = 1;
+        while (i < arc)
+        {
+            if (ft_get_num(argv[i], &a) == -1)
+            {
+                return (-1);
+            }
+            i++;
+        }
+        
+        b = a;
+        while (b)
+        {
+			//ft_free(a);
+            printf("%i\n", b->value);
+            b = b->next;
+        }
+    }
+    return (0);
 }
--- parse.c
+++ parse.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jopajuel <jopajuel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aitorres <aitorres@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 09:36:21 by jopajuel          #+#    #+#             */
-/*   Updated: 2026/02/10 12:32:12 by jopajuel         ###   ########.fr       */
+/*   Updated: 2026/02/10 18:25:34 by aitorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
+
 void	ft_itoa_pro(char **str, t_list **a, int index)
 {
 	int		i;
@@ -29,28 +30,63 @@ void	ft_itoa_pro(char **str, t_list **a, int index)
 }
 
 // "    123  "
+// int	ft_get_num(char *list_num, t_list **a)
+// {
+// 	int	index;
+
+// 	index = 0;
+// 	while (*list_num)
+// 	{
+// 		if (*list_num >= '\t' && *list_num <= '\r')
+// 		{//printf("%c\n", *list_num);
+// 			return (ft_error());
+// 		}
+// 		if (*list_num != ' ' && !ft_isdigit(*list_num))
+// 		{//printf("%c\n", *list_num);
+// 			return (ft_error());
+// 		}
+// 		if (*list_num >= '0' && *list_num <= '9')
+// 		{
+// 			//printf("%i\n", index);
+// 			ft_itoa_pro(&list_num, a, index);
+// 		}
+// 		if (*list_num)
+// 			list_num++;
+// 		index++;
+// 	}
+// 	return (index);
+// }
+
+
 int	ft_get_num(char *list_num, t_list **a)
 {
+	int		i_comi;
 	int	index;
 
 	index = 0;
+
+	i_comi = 0;
 	while (*list_num)
 	{
-		if (*list_num >= '\t' && *list_num <= '\r')
-		{//printf("%c\n", *list_num);
-			return (ft_error());
+		while (*list_num)
+		{
+			if (ft_isdigit(*list_num) || (*list_num == '-' && ft_isdigit(*(list_num + 1))))
+				break ;
+			if (*list_num != ' ' && *list_num != '"' && *list_num != '-')
+				return (ft_error());
+			if (*list_num == '"')
+				i_comi++;
+			list_num++;
 		}
-		if (*list_num != ' ' && !ft_isdigit(*list_num))
-		{//printf("%c\n", *list_num);
-			return (ft_error());
-		}
-		if (*list_num >= '0' && *list_num <= '9')
-		{//printf("%i\n", index);
+
+		
+		if (*list_num)
+		{
 			ft_itoa_pro(&list_num, a, index);
 		}
-		if (*list_num)
-			list_num++;
 		index++;
 	}
+	if (i_comi % 2 != 0)
+		return (ft_error());
 	return (index);
 }
